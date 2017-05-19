044ca338fc51d2fdcc393180157e09938b7d1da1
Ronan Letellier <ronanlet@gmail.com>
2017-05-19 11:19:00 +0200

    Restore site old version

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,50 +1,46 @@
-(function() {
-    window.onload = function() {
+$(document).ready(function() {
+  // Google Map iframe size
+  $("a.iframe").fancybox({
+    width: 900,
+    height: 600
+  });
 
-    var interval;
+  $("#keyboardTip").find(".close").first().click(function() {
+    $("#keyboardTip").remove();
 
-    // Navigation
-    document.querySelectorAll('.nav-item').forEach(function(el) {
-      el.onclick = function(ev) {
-        ev.preventDefault();
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + 30);
 
-        if (interval) clearInterval(interval);
+    document.cookie = "keytip=true;expires=" + exdate.toGMTString();
+  });
 
-        var target = document.querySelector('#' + this.getAttribute('data-target'));
-        scrollTo(target, 500);
-      };
-    });
-
-  }
-
-  function scrollTo(element, duration) {
-    var e = document.documentElement;
-    if(e.scrollTop === 0) {
-      var t = e.scrollTop;
-      ++e.scrollTop;
-      e = t+1 === e.scrollTop -- ? e : document.body;
+  // Keyboard navigation
+  combos = [
+    {
+      "keys": "k",
+      "is_exclusive": true,
+      "on_keyup": function() {
+        var nextPage = $("#nav").find(".active").first().next().find("a").first();
+        window.location.href = typeof(nextPage.attr("href")) !== "undefined" ? nextPage.attr("href") : "home";
+      }
+    },
+    {
+      "keys": "j",
+      "is_exclusive": true,
+      "on_keyup": function() {
+        var previousPage = $("#nav").find(".active").first().prev().find("a").first();
+        window.location.href = typeof(previousPage.attr("href")) !== "undefined" ? previousPage.attr("href") : "contact";
+      }
     }
-    scrollToC(e, e.scrollTop, element, duration);
-  }
+  ];
+  keypress.register_many(combos);
+});
 
-  function scrollToC(element, from, to, duration) {
-    if (duration < 0) return;
-    if(typeof from === "object") from = from.offsetTop;
-    if(typeof to === "object") to = to.offsetTop;
-    scrollToX(element, from, to, 0, 1 / duration, 20, easeOut);
-  }
+/*
+     FILE ARCHIVED ON 12:13:37 Oct 16, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:29:47 May 19, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
 
-  function scrollToX(element, x1, x2, t, v, step, operation) {
-    if (t < 0 || t > 1 || v <= 0) return;
-    element.scrollTop = x1 - (x1 - x2) * operation(t);
-    t += v * step;
-    setTimeout(function() {
-      scrollToX(element, x1, x2, t, v, step, operation);
-    }, step);
-  }
-
-  function easeOut(t){
-    t--;
-    return t * t * t + 1;
-  }
-})();
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
